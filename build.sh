f80a08015387787ab009aa284edd217fc57e62f6
Andrew Rothstein <arothste@blackrock.com>
2015-10-16 01:06:20 -0400

    first cut. got the ubuntu_trusty build rocking. the fedora_21 build is choking on where to find boost. probably because it's in /usr/lib64 instead of /usr/lib.

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#docker build -t andrewrothstein/docker-quantlib-builder:fedora_21 fedora_21
+docker build -t andrewrothstein/docker-quantlib-builder:ubuntu_trusty ubuntu_trusty
