802c5f8067320deafda367c5ec325ed78a3275ac
Andrew Rothstein <arothste@blackrock.com>
2015-10-19 00:45:30 -0400

    refactored out the app name

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-#docker build -t andrewrothstein/docker-quantlib-builder:fedora_21 fedora_21
-docker build -t andrewrothstein/docker-quantlib-builder:ubuntu_trusty ubuntu_trusty
+export APP_NAME=quantlib
+#docker build -t andrewrothstein/docker-$APP_NAME-builder:fedora_21 fedora_21
+docker build -t andrewrothstein/docker-$APP_NAME-builder:ubuntu_trusty ubuntu_trusty
